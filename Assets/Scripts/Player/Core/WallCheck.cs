c64fea1bed961831d77b114e470e5caa19e0b647
NewNomad <tosiki1997@gmail.com>
2023-12-23 12:18:22 +0900

    Change layer of GameObject "Wall" to 6 and add "Wall" tag

---
+++ Assets/Scripts/Player/Core/WallCheck.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+namespace Game.Core
+{
+    public class WallCheck : MonoBehaviour
+    {
+
+        [Header("Ground Check")]
+        [SerializeField] LayerMask wallLayer;
+        [SerializeField] float wallCheckDistance = 0.1f;
+        [SerializeField] float groundCheckDistance = 0.1f;
+
+        public bool IsTouchingGround()
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, wallLayer);
+            return hit.collider != null;
+        }
+
+        public bool IsTouchingWall()
+        {
+            RaycastHit2D hitRight = Physics2D.Raycast(transform.position, Vector2.right, wallCheckDistance, wallLayer);
+            if (hitRight.collider != null)
+            {
+                return true;
+            }
+            else
+            {
+                RaycastHit2D hitLeft = Physics2D.Raycast(transform.position, Vector2.left, wallCheckDistance, wallLayer);
+                return hitLeft.collider != null;
+            }
+        }
+    }
+}
