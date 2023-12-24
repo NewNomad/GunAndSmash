784d20d7f826485a25141ed570f83a317ccf8145
NewNomad <tosiki1997@gmail.com>
2023-12-24 17:11:05 +0900

    feat: 被弾時の無敵とhitstopを追加

--- Assets/Scripts/Player/Core/Health.cs
+++ Assets/Scripts/Player/Core/Health.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 namespace Game.Core
 {
@@ -5,11 +6,13 @@ namespace Game.Core
     {
         [SerializeField] private int maxHealth = 5;
         private int health;
-        [SerializeField] float invincibleTime = 1f;
+        [SerializeField] float invincibleTime = 1.5f;
         private float currentInvincibleTime = 0f;
+        SpriteRenderer spriteRenderer;
 
         private void Awake()
         {
+            TryGetComponent(out spriteRenderer);
             health = maxHealth;
         }
 
@@ -21,8 +24,10 @@ namespace Game.Core
         public void TakeDamage(int damage)
         {
             if (currentInvincibleTime < invincibleTime) return;
+            HitStopController.Instance.HitStop();
             currentInvincibleTime = 0f;
             health -= damage;
+            StartCoroutine(InvincibleBlink());
             Debug.Log("Health: " + health);
             if (health <= 0)
             {
@@ -34,6 +39,23 @@ namespace Game.Core
         {
             // TODO: 死亡時の処理
         }
+
+        // 無敵時の点滅
+        private IEnumerator InvincibleBlink()
+        {
+            const float blinkColorValue = 0.5f;
+            Color defaultColor = spriteRenderer.color;
+            Color blinkColor = new Color(defaultColor.r * blinkColorValue, defaultColor.g * blinkColorValue, defaultColor.b * blinkColorValue);
+
+            while (currentInvincibleTime < invincibleTime)
+            {
+                spriteRenderer.color = blinkColor;
+                yield return new WaitForSeconds(0.1f);
+                spriteRenderer.color = defaultColor;
+                yield return new WaitForSeconds(0.1f);
+            }
+            spriteRenderer.color = defaultColor;
+            yield return null;
+        }
     }
 }
-
