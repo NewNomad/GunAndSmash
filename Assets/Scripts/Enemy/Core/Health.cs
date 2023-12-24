99edfa69859d20703d2841e0d199fb7491223246
NewNomad <tosiki1997@gmail.com>
2023-12-24 15:37:06 +0900

    feat: stungauge追加

--- Assets/Scripts/Enemy/Core/Health.cs
+++ Assets/Scripts/Enemy/Core/Health.cs
@@ -8,30 +8,38 @@ namespace Enemy.Core
     {
         [SerializeField] private int maxHealth = 280;
         private int health;
+        [SerializeField] private int stunMaxHealth = 180;
+        private int stunHealth;
         FlashObject flashObject;
         [SerializeField] ParticleSystem stunParticles;
 
         private void Awake()
         {
             health = maxHealth;
+            stunHealth = stunMaxHealth;
             TryGetComponent(out flashObject);
         }
 
         public void TakeDamage(int damage)
         {
             health -= damage;
-            Debug.Log("Enemy health: " + health);
+            stunHealth -= damage;
             SetEnemyFlash();
-            if (health <= 0)
+            if (stunHealth <= 0)
             {
                 Debug.Log("Enemy is dead");
                 Instantiate(stunParticles, transform.position, Quaternion.identity);
                 HitStopController.Instance.HitStop();
                 // TODO: DIE()
             }
+
         }
 
-
+        private void FixedUpdate()
+        {
+            stunHealth += 1;
+            stunHealth = Mathf.Clamp(stunHealth, 0, stunMaxHealth);
+        }
 
 
         private void SetEnemyFlash()
