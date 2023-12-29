81d26b0019830721c91c8dbb831f9d4d7398e264
NewNomad <tosiki1997@gmail.com>
2023-12-30 08:50:00 +0900

    Update PowerOrbBlue prefab scaling mode and add delay to StartGameItem

--- Assets/Scripts/Item/StartGameItem.cs
+++ Assets/Scripts/Item/StartGameItem.cs
@@ -1,8 +1,17 @@
+using System.Collections;
 using UnityEngine;
 
 public class StartGameItem : MonoBehaviour, IItem
 {
     public void UseItem()
     {
+        StartCoroutine(StartGame());
+    }
+
+    IEnumerator StartGame()
+    {
+        Time.timeScale = .5f;
+        yield return new WaitForSecondsRealtime(1f);
+        Time.timeScale = 0f;
     }
 }
